2dd2fc7fbe7a73e4624192f78d6376ab54156a35
Nolan Crane <nolancrane@yahoo.com>
2018-02-09 18:26:06 -0500

    - Adding in Main.java and WikiTopographyTests.java

---
+++ src/wikiTopography/Main.java
@@ -0,0 +1,9 @@
+package wikiTopography;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
---
+++ src/wikiTopography/WikiTopographyTests.java
@@ -0,0 +1,13 @@
+package wikiTopography;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class WikiTopographyTests {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
